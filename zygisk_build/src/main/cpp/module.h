e93106d0f33454a71403443d3dd2ad6c0a5ccc99
MONA <121730102+MeowDump@users.noreply.github.com>
2026-05-05 09:16:21 +0530

    Rename namespace
    Signed-off-by: MONA <121730102+MeowDump@users.noreply.github.com>

--- zygisk_build/src/main/cpp/module.h
+++ zygisk_build/src/main/cpp/module.h
@@ -5,10 +5,10 @@
 #include <android/log.h>
 #include <sys/stat.h>
 
-namespace pixelifytag {
+namespace meowfytag {
 
 #ifndef TAG
-#define TAG    "Pixelify"
+#define TAG    "Meowfy"
 #endif
 
 // Check debug flag once at module load
@@ -20,10 +20,10 @@ static inline void init_debug() {
     g_debug_enabled = (stat("/data/adb/modules/UnlimitedPhotosStorage/debug", &st) == 0);
 }
 
-#define LOGD(...) do { if (pixelifytag::g_debug_enabled) __android_log_print(ANDROID_LOG_DEBUG, TAG, __VA_ARGS__); } while(0)
-#define LOGI(...) do { if (pixelifytag::g_debug_enabled) __android_log_print(ANDROID_LOG_INFO,  TAG, __VA_ARGS__); } while(0)
-#define LOGW(...) do { if (pixelifytag::g_debug_enabled) __android_log_print(ANDROID_LOG_WARN,  TAG, __VA_ARGS__); } while(0)
-#define LOGE(...) do { if (pixelifytag::g_debug_enabled) __android_log_print(ANDROID_LOG_ERROR, TAG, __VA_ARGS__); } while(0)
-#define LOGERRNO(...) do { if (pixelifytag::g_debug_enabled) __android_log_print(ANDROID_LOG_ERROR, TAG, __VA_ARGS__ ": %d (%s)", errno, strerror(errno)); } while(0)
+#define LOGD(...) do { if (meowfytag::g_debug_enabled) __android_log_print(ANDROID_LOG_DEBUG, TAG, __VA_ARGS__); } while(0)
+#define LOGI(...) do { if (meowfytag::g_debug_enabled) __android_log_print(ANDROID_LOG_INFO,  TAG, __VA_ARGS__); } while(0)
+#define LOGW(...) do { if (meowfytag::g_debug_enabled) __android_log_print(ANDROID_LOG_WARN,  TAG, __VA_ARGS__); } while(0)
+#define LOGE(...) do { if (meowfytag::g_debug_enabled) __android_log_print(ANDROID_LOG_ERROR, TAG, __VA_ARGS__); } while(0)
+#define LOGERRNO(...) do { if (meowfytag::g_debug_enabled) __android_log_print(ANDROID_LOG_ERROR, TAG, __VA_ARGS__ ": %d (%s)", errno, strerror(errno)); } while(0)
 
 }
