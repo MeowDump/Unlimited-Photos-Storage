63f32290755a496db2bf7a3eb90c07313d96cd48
MONA <121730102+MeowDump@users.noreply.github.com>
2026-05-05 09:22:52 +0530

    Simplify logging macros
    Signed-off-by: MONA <121730102+MeowDump@users.noreply.github.com>

--- zygisk_build/src/main/cpp/module.h
+++ zygisk_build/src/main/cpp/module.h
@@ -11,19 +11,17 @@ namespace meowfytag {
 #define TAG    "Meowfy"
 #endif
 
-// Check debug flag once at module load
 extern bool g_debug_enabled;
 
-// Initialize debug state
 static inline void init_debug() {
     struct stat st;
     g_debug_enabled = (stat("/data/adb/modules/UnlimitedPhotosStorage/debug", &st) == 0);
 }
 
+// Only use debug flag for verbose logs
 #define LOGD(...) do { if (meowfytag::g_debug_enabled) __android_log_print(ANDROID_LOG_DEBUG, TAG, __VA_ARGS__); } while(0)
-#define LOGI(...) do { if (meowfytag::g_debug_enabled) __android_log_print(ANDROID_LOG_INFO,  TAG, __VA_ARGS__); } while(0)
-#define LOGW(...) do { if (meowfytag::g_debug_enabled) __android_log_print(ANDROID_LOG_WARN,  TAG, __VA_ARGS__); } while(0)
-#define LOGE(...) do { if (meowfytag::g_debug_enabled) __android_log_print(ANDROID_LOG_ERROR, TAG, __VA_ARGS__); } while(0)
-#define LOGERRNO(...) do { if (meowfytag::g_debug_enabled) __android_log_print(ANDROID_LOG_ERROR, TAG, __VA_ARGS__ ": %d (%s)", errno, strerror(errno)); } while(0)
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, TAG, __VA_ARGS__)
+#define LOGW(...) __android_log_print(ANDROID_LOG_WARN, TAG, __VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, TAG, __VA_ARGS__)
 
-}
+} // namespace meowfytag
